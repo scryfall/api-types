3d8a7962eebf9bdfffc6afbcad6832e81a1121ef
Scarlett <4585677+scarletcs@users.noreply.github.com>
2023-11-27 01:51:52 +0000

    add test coverage for ScryfallCard.Unknown

--- __test__/global.d.ts
+++ __test__/global.d.ts
@@ -2,5 +2,7 @@ import { ScryfallCard } from "../src";
 import { ScryfallObject } from "../src/objects/Object";
 
 declare global {
-  type TestCard<T extends ScryfallObject.Object<ScryfallObject.ObjectType.Card>> = T & ScryfallCard.Any;
+  type TestCard<T extends ScryfallObject.Object<ScryfallObject.ObjectType.Card>> = T &
+    ScryfallCard.Any &
+    ScryfallCard.Unknown;
 }
