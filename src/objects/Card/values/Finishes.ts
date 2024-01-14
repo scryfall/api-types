48894d55e07f97d67ee2b21acdc0617d4dc0fe0d
Andrew McKnight <amcknight2718@gmail.com>
2024-01-14 01:25:28 -0900

    add `scroll` to Finishes.ts

--- src/objects/Card/values/Finishes.ts
+++ src/objects/Card/values/Finishes.ts
@@ -62,6 +62,10 @@ export enum ScryfallExtendedFinish {
    * @trivia The name was a reference to "step and repeat", a style of pattern used in printing banners.
    */
   StepAndCompleat = "stepandcompleat",
+  /**
+   * A Lord of the Rings finish that made the card name, type line and oracle text area appear as scrolls.
+   */
+  Scroll = "scroll",
 }
 
 export type ScryfallExtendedFinishLike = ScryfallExtendedFinish | `${ScryfallExtendedFinish}`;
