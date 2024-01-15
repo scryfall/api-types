2001495828c2bfa496a2b4b2466c1d0aa4ce3285
Andrew McKnight <amcknight2718@gmail.com>
2024-01-15 14:39:13 -0900

    add `T` to ManaType.ts

--- src/objects/Card/values/ManaType.ts
+++ src/objects/Card/values/ManaType.ts
@@ -5,6 +5,8 @@ enum ScryfallManaType {
   Red = "R",
   Green = "G",
   Colorless = "C",
+  /** There is a card that produces a "tap" that can be used to activate a tapped ability on a card without tapping it; see https://scryfall.com/card/24649a26-822e-456f-8f28-8e1d002fdd81. */
+  Tap = "T", 
 }
 
 export type ScryfallManaTypeLike = ScryfallManaType | `${ScryfallManaType}`;
