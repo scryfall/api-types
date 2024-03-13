7645b616136678035386146be7a67b851060e16d
scarletcs <4585677+scarletcs@users.noreply.github.com>
2024-03-13 00:35:34 +0000

    fix legalities entry

--- src/objects/Card/values/LegalitiesField.ts
+++ src/objects/Card/values/LegalitiesField.ts
@@ -1,4 +1,4 @@
 import { ScryfallFormat } from "./Format";
 import { ScryfallLegality } from "./Legality";
 
-export type ScryfallLegalitiesField = Record<ScryfallFormat, ScryfallLegality>;
+export type ScryfallLegalitiesField = Record<`${ScryfallFormat}`, `${ScryfallLegality}`>;
