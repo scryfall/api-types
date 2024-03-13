11284c74ccd2d589125cdfe2c222bacc352970db
Scarlet <4585677+scarletcs@users.noreply.github.com>
2024-03-13 01:37:31 +0000

    Remove old test files (#36)
    * nuke old test folder
    
    * update package config

--- __test__/global.d.ts
+++
@@ -1,5 +0,0 @@
-import { ScryfallCard, ScryfallObject } from "../src";
-
-declare global {
-  type TestCard<T extends ScryfallObject.Object<ScryfallObject.ObjectType.Card>> = T & ScryfallCard.Any;
-}
